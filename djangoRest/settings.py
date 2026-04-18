fa74d159412ed8948729824b9630d7fb17151774
Your Name <rifatsarkercse@gmail.com>
2026-04-18 23:34:50 +0600

    added media

--- djangoRest/settings.py
+++ djangoRest/settings.py
@@ -148,3 +148,6 @@ EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'rifatsarker505@gmail.com'
 EMAIL_HOST_PASSWORD = 'ggtb tjro gpkr vjlv'  # not normal password
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
